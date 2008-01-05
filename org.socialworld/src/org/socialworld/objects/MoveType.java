b59613a64cbca62b36e1dcd3782ac2c229f30806
André Schade <andre.schade@limbus-medtec.com>
2008-01-05 21:53:31 +0000

    Formatierungen durchgeführt

--- org.socialworld/src/org/socialworld/objects/MoveType.java
+++ org.socialworld/src/org/socialworld/objects/MoveType.java
@@ -5,29 +5,29 @@ package org.socialworld.objects;
 
 /**
  * @author Mathias Sikos (tyloesand)
- *
+ * 
  */
 public class MoveType {
-	
-	int type;
-	
-	public MoveType() {
-	
-	}
 
-	/**
-	 * @return the type
-	 */
-	public int getType() {
-		return type;
-	}
+    int type;
 
-	/**
-	 * @param type the type to set
-	 */
-	public void setType(int type) {
-		this.type = type;
-	}
-	
-	
+    public MoveType() {
+
+    }
+
+    /**
+     * @return the type
+     */
+    public int getType() {
+	return type;
+    }
+
+    /**
+     * @param type
+     *                the type to set
+     */
+    public void setType(int type) {
+	this.type = type;
+    }
+
 }
