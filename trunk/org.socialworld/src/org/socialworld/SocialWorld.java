dbeb7b1f66f26f3f05d51dc559edca5038c8e804
circlesmiler <circlesmiler@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2008-01-05 20:17:55 +0000

    Integrate Log4J

--- trunk/org.socialworld/src/org/socialworld/SocialWorld.java
+++ trunk/org.socialworld/src/org/socialworld/SocialWorld.java
@@ -3,18 +3,21 @@
  */
 package org.socialworld;
 
+import org.apache.log4j.Logger;
+
 /**
  * @author Mathias Sikos (tyloesand)
- *
+ * 
  */
 public class SocialWorld {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static Logger logger = Logger.getLogger(SocialWorld.class);
 
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	logger.info("Start main method of SocialWord");
+    }
 
 }
