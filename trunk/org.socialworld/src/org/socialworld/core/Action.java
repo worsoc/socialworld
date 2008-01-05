0745d2e7bd4394a8322fd5e923cec65213894e6d
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2008-01-05 21:07:44 +0000

    further attributes incl. setter and getter methods added

--- trunk/org.socialworld/src/org/socialworld/core/Action.java
+++ trunk/org.socialworld/src/org/socialworld/core/Action.java
@@ -3,14 +3,38 @@
  */
 package org.socialworld.core;
 
+import org.socialworld.objects.Direction;
+import org.socialworld.objects.SimulationObject;
+
 /**
  * @author Mathias Sikos (tyloesand)
  *
  */
 public class Action {
 	
+	private	int type;
 	private int priority;
+	private SimulationObject target;
+	private Direction direction;
+	private	int mode;
+	private double intensity;
+	
 	private double remainedDuration;
+
+	/**
+	 * The methods returns the action type.
+	 * @return type
+	 */	
+	public int getType() {
+			return type;
+		}
+
+/**
+	 * @param type the type to set
+	 */
+	public void setType(int type) {
+		this.type = type;
+	}
 
 /**
  * The methods returns the action's priority.
@@ -19,7 +43,70 @@ public class Action {
 	public int getPriority() {
 		return priority;
 	}
-	
+
+/**
+ * @param priority the priority to set
+ */
+public void setPriority(int priority) {
+	this.priority = priority;
+}
+
+/**
+	 * @return the target
+	 */
+	public SimulationObject getTarget() {
+		return target;
+	}
+
+	/**
+	 * @param target the target to set
+	 */
+	public void setTarget(SimulationObject target) {
+		this.target = target;
+	}
+
+	/**
+	 * @return the direction
+	 */
+	public Direction getDirection() {
+		return direction;
+	}
+
+	/**
+	 * @param direction the direction to set
+	 */
+	public void setDirection(Direction direction) {
+		this.direction = direction;
+	}
+
+	/**
+	 * @return the mode
+	 */
+	public int getMode() {
+		return mode;
+	}
+
+	/**
+	 * @param mode the mode to set
+	 */
+	public void setMode(int mode) {
+		this.mode = mode;
+	}
+
+	/**
+	 * @return the intensity
+	 */
+	public double getIntensity() {
+		return intensity;
+	}
+
+	/**
+	 * @param intensity the intensity to set
+	 */
+	public void setIntensity(double intensity) {
+		this.intensity = intensity;
+	}
+
 	/**
 	 * The methods returns the action's remained duration.
 	 * @return remainedDuration
@@ -28,6 +115,13 @@ public class Action {
 		return remainedDuration;
 	}
 	
+/**
+ * @param remainedDuration the remainedDuration to set
+ */
+public void setRemainedDuration(double remainedDuration) {
+	this.remainedDuration = remainedDuration;
+}
+
 /**
  * The methods decrements the attribute remainedDuration.
  * That means, an action needs less time to complete.
