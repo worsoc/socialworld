6b172bd5f819d096bf855761eec0e1fee3aa681e
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2008-01-05 21:03:58 +0000

    creation

---
+++ trunk/org.socialworld/src/org/socialworld/objects/MoveType.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.socialworld.objects;
+
+/**
+ * @author Mathias Sikos (tyloesand)
+ *
+ */
+public class MoveType {
+	
+	int type;
+	
+	public MoveType() {
+	
+	}
+
+	/**
+	 * @return the type
+	 */
+	public int getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(int type) {
+		this.type = type;
+	}
+	
+	
+}
