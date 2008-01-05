a144eebdce0317d8ede062776819e016eea4b02c
André Schade <andre.schade@limbus-medtec.com>
2008-01-05 20:38:48 +0000

    JUnit Example Test added

---
+++ org.socialworld/test/org/socialworld/core/test/ActionHandlerTest.java
@@ -0,0 +1,39 @@
+package org.socialworld.core.test;
+
+import junit.framework.TestCase;
+
+public class ActionHandlerTest extends TestCase {
+
+    public ActionHandlerTest(String name) {
+	super(name);
+    }
+
+    protected void setUp() throws Exception {
+	super.setUp();
+    }
+
+    protected void tearDown() throws Exception {
+	super.tearDown();
+    }
+
+    public void testDoAction() {
+	fail("Not yet implemented");
+    }
+
+    public void testDoActualAction() {
+	fail("Not yet implemented");
+    }
+
+    public void testInsertAction() {
+	fail("Not yet implemented");
+    }
+
+    public void testNewAction() {
+	fail("Not yet implemented");
+    }
+
+    public void testChangeAction() {
+	fail("Not yet implemented");
+    }
+
+}
