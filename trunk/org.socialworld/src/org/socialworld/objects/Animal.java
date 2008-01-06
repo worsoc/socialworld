afd7ea989d6961307e7bcb4c86446d5d5c88e93e
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2008-01-06 21:02:40 +0000

    action implementation moved to simulation object. so ActionHandler isn't abstract any more. The action handler calls doAction() in the simulation object.

--- trunk/org.socialworld/src/org/socialworld/objects/Animal.java
+++ trunk/org.socialworld/src/org/socialworld/objects/Animal.java
@@ -3,6 +3,9 @@
  */
 package org.socialworld.objects;
 
+import org.socialworld.core.Action;
+import org.socialworld.core.ActionType;
+
 /**
  * 
  * @author Mathias Sikos (tyloesand)
@@ -10,35 +13,105 @@ package org.socialworld.objects;
  */
 public class Animal extends SimulationObject {
 
-    protected Move move;
+	protected Move move;
 
-    public Animal() {
-	super();
-    }
+	public Animal() {
+		super();
+	}
 
-    /**
-     * @return the Move
-     */
-    public Move getMove() {
-	return move;
-    }
+	/**
+	 * Depending on the action type the method calls the according procedure
+	 * with special implementation of the action.
+	 */
+	public void doAction(Action action) {
 
-    /**
-     * @param Move
-     *                the Move to set
-     */
-    public void setMove(Move move) {
-	this.move = move;
-    }
+		ActionType type;
+		type = action.getType();
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.socialworld.objects.SimulationObject#determineInfluence(org.socialworld.objects.SimulationEvent)
-     */
-    @Override
-    public void determineInfluence(SimulationEvent simualationEvent) {
+		switch (type) {
+		case sleep:
+			sleep(action);
+			return;
+		case changeMove:
+			changeMove(action);
+			return;
+		case kick:
+			kick(action);
+			return;
+		case move:
+			move(action);
+			return;
+		case say:
+		default:
+			return;
+		}
+	}
 
-    }
+	/**
+	 * The method changes the move mode of an animal.
+	 * 
+	 * @param action
+	 */
+	protected void changeMove(Action action) {
+		move.setMode(action.getMode());
+	}
+
+	/**
+	 * The method holds the implementation of moving an animal.
+	 * 
+	 * @param action
+	 */
+	protected void move(Action action) {
+	}
+
+	/**
+	 * The method holds the implementation of kicking.
+	 * 
+	 * @param action
+	 */
+	protected void kick(Action action) {
+	}
+
+	/**
+	 * The method holds the implementation of sleep.
+	 * 
+	 * @param action
+	 */
+	protected void sleep(Action action) {
+		ActionMode mode = action.getMode();
+
+		switch (mode) {
+		case sleepIntentioned:
+			break;
+		case sleepCaused:
+			break;
+		default:
+		}
+	}
+
+	/**
+	 * @return the Move
+	 */
+	public Move getMove() {
+		return move;
+	}
+
+	/**
+	 * @param Move
+	 *            the Move to set
+	 */
+	public void setMove(Move move) {
+		this.move = move;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.socialworld.objects.SimulationObject#determineInfluence(org.socialworld.objects.SimulationEvent)
+	 */
+	@Override
+	public void determineInfluence(SimulationEvent simualationEvent) {
+
+	}
 
 }
--- trunk/org.socialworld/src/org/socialworld/objects/Human.java
+++ trunk/org.socialworld/src/org/socialworld/objects/Human.java
@@ -3,6 +3,9 @@
  */
 package org.socialworld.objects;
 
+import org.socialworld.core.Action;
+import org.socialworld.core.ActionType;
+
 /**
  * @author Mathias Sikos (tyloesand)
  * 
@@ -13,6 +16,89 @@ public class Human extends Mammal {
 
 	public Human() {
 		super();
+	}
+
+	/**
+	 * Depending on the action type the method calls the according procedure
+	 * with special implementation of the action.
+	 */
+	public void doAction(Action action) {
+
+		ActionType type;
+		type = action.getType();
+
+		switch (type) {
+		case useFastItem:
+			useFastItem(action);
+			return;
+		case touch:
+			touch(action);
+			return;
+		case sleep:
+			sleep(action);
+			return;
+		case changeMove:
+			changeMove(action);
+			return;
+		case kick:
+			kick(action);
+			return;
+		case controlHandManually:
+			controlHandManually(action);
+			return;
+		case spell:
+			spell(action);
+			return;
+		case useWeaponLeft:
+			useWeaponLeft(action);
+			return;
+		case useWeaponRight:
+			useWeaponRight(action);
+			return;
+		case move:
+			move(action);
+			return;
+		case say:
+			say(action);
+			return;
+		case useItem:
+			useItem(action);
+			return;
+		default:
+			return;
+		}
+	}
+
+	protected void useItem(Action action) {
+
+	}
+
+	protected void say(Action action) {
+
+	}
+
+	protected void useWeaponRight(Action action) {
+
+	}
+
+	protected void useWeaponLeft(Action action) {
+
+	}
+
+	protected void spell(Action action) {
+
+	}
+
+	protected void controlHandManually(Action action) {
+
+	}
+
+	protected void touch(Action action) {
+
+	}
+
+	protected void useFastItem(Action action) {
+
 	}
 
 	/**
