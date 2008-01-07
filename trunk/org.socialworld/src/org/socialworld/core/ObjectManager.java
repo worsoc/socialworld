c229de94f4c346f968437d831d1e22b4a8f45771
circlesmiler <circlesmiler@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2008-01-07 21:37:22 +0000

    Kleinen Fehler in "Action" gefixt und paar Fragen (Kommentare) eingefügt.

--- trunk/org.socialworld/src/org/socialworld/core/ObjectManager.java
+++ trunk/org.socialworld/src/org/socialworld/core/ObjectManager.java
@@ -9,33 +9,33 @@ package org.socialworld.core;
  */
 public class ObjectManager {
 
-    private static ObjectManager manager = null;
+	private static ObjectManager manager = null;
 
-    Simulation simulation;
+	Simulation simulation;
 
-    private ObjectManager() {
-	simulation = new Simulation();
-    }
-
-    /**
-     * Returns the {@link ObjectManager} instance.
-     * 
-     * @return {@link ObjectManager} instance
-     */
-    public static ObjectManager getObjectManager() {
-	if (manager == null) {
-	    manager = new ObjectManager();
+	private ObjectManager() {
+		simulation = new Simulation();
 	}
-	return manager;
-    }
 
-    public Simulation getSimulation() {
-	return simulation;
-    }
+	/**
+	 * Returns the {@link ObjectManager} instance.
+	 * 
+	 * @return {@link ObjectManager} instance
+	 */
+	public static ObjectManager getObjectManager() {
+		if (manager == null) {
+			manager = new ObjectManager();
+		}
+		return manager;
+	}
 
-    public EventMaster getEventMaster() {
-	return simulation.getEventMaster();
-    }
+	public Simulation getSimulation() {
+		return simulation;
+	}
+
+	public EventMaster getEventMaster() {
+		return simulation.getEventMaster();
+	}
 
 }
 
