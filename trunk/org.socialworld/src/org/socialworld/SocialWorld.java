d9c5957ce83b72ce4ae7568d332c224ed6196fe1
circlesmiler <circlesmiler@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2008-02-19 19:07:54 +0000

    - Stop der Simulation implementiert - Bundle wird nun automatisch gestartet, wenn eine Klasse des Bundles benutzt werden soll. (LazyLoading)

--- trunk/org.socialworld/src/org/socialworld/SocialWorld.java
+++ trunk/org.socialworld/src/org/socialworld/SocialWorld.java
@@ -3,53 +3,44 @@
  */
 package org.socialworld;
 
-import java.util.List;
-
 import org.apache.log4j.Logger;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.socialworld.core.Simulation;
-import org.socialworld.objects.Human;
 
 /**
  * @author Mathias Sikos (tyloesand)
  * 
  */
 public class SocialWorld implements BundleActivator {
-
-    private static final Logger logger = Logger.getLogger(SocialWorld.class);
-    
-    private static SocialWorld currentObject;
 	
-    private Simulation simulation;
+	public static final String PLUGIN_ID = "org.socialworld";
 
-    public void start(BundleContext context) throws Exception {
-	logger.info("Start main method of SocialWorld");
-	
-	currentObject = this;
-	
-	simulation = new Simulation();
-	simulation.startSimulation();
-	
-	List<Human> humans = simulation.getHumans();
-	humans.add(new Human());
-	humans.add(new Human());
-	humans.add(new Human());
+	private static final Logger logger = Logger.getLogger(SocialWorld.class);
 
-	System.out.println("Humans: " + humans);
+	private static SocialWorld currentObject;
 
-	logger.info("End main method of SocialWorld");
-    }
+	private Simulation simulation;
 
-    public void stop(BundleContext context) throws Exception {
-	// TODO Simulation stoppen, Threads beenden, Ressourcen freigeben.
-    }
-    
-    public static SocialWorld getCurrent() {
-    	return currentObject;
-    }
-    
-    /**
+	public void start(BundleContext context) throws Exception {
+		logger.info("Start bundle " + PLUGIN_ID);
+
+		currentObject = this;
+		simulation = new Simulation();
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		// TODO Simulation stoppen, Threads beenden, Ressourcen freigeben.
+		currentObject = null;
+		simulation.stopSimulation();
+		simulation = null;
+	}
+
+	public static SocialWorld getCurrent() {
+		return currentObject;
+	}
+
+	/**
 	 * @return the simulation
 	 */
 	public Simulation getSimulation() {
