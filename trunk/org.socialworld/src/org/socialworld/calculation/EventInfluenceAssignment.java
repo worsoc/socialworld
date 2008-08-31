e8c1c6da5c0238bc52b6c703b43e055e84799863
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2008-08-31 21:50:02 +0000

    created

---
+++ trunk/org.socialworld/src/org/socialworld/calculation/EventInfluenceAssignment.java
@@ -0,0 +1,42 @@
+package org.socialworld.calculation;
+
+
+public class EventInfluenceAssignment {
+
+	private static EventInfluenceAssignment eventInfluenceAssignment;
+	
+	/**
+	 * Because of being a singleton the instance is created in a private
+	 * constructor.
+	 */
+	private EventInfluenceAssignment() {
+	}
+
+	/**
+	 * The method gets back the only instance of the EventInfluenceAssignment.
+	 * 
+	 * @return singleton object of EventInfluenceAssignment
+	 */
+	public static EventInfluenceAssignment getInstance() {
+		if (eventInfluenceAssignment == null) {
+			eventInfluenceAssignment = new EventInfluenceAssignment();
+		}
+		return eventInfluenceAssignment;
+	}
+
+	/**
+	 * The method gets back the description how the event effects to the object.
+	 * The description depends on the event type and the type of the influence to the object
+	 * 
+	 * @param eventTyp
+	 * @param influenceType	 
+	 * @return EventInfluenceDescription
+	 */
+	public EventInfluenceDescription getEventInfluenceDescription(
+			int eventType,	int influenceType) {
+		EventInfluenceDescription eventInfluenceDescription;
+		// TODO (tyloesand) eventInfluenceDescription mit Wert belegen 
+		//return eventInfluenceDescription;
+		return null;
+	}
+}
