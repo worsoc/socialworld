fc493d424bba7ae58b645b8674b6514f00881281
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2012-01-08 21:22:36 +0000

    calculate() and initialize() implemented in dependence on AttributeCalculatorFunctionTabeleType

---
+++ trunk/org.socialworld/src/org/socialworld/calculation/AttributeCalculatorFunctionTableType.java
@@ -0,0 +1,7 @@
+package org.socialworld.calculation;
+
+public enum AttributeCalculatorFunctionTableType {
+	identical, positive_raise, negative_raise, 
+	v, v_mirrored, u, u_mirrored, horizontal_min, horizontal_max
+
+}
