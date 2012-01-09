0c497311b92b7d8321ace5258fa4a8c0f2645a00
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2012-01-09 23:43:48 +0000

    initialize() implemented

--- org.socialworld/src/org/socialworld/calculation/AttributeCalculatorFunctions.java
+++ org.socialworld/src/org/socialworld/calculation/AttributeCalculatorFunctions.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class AttributeCalculatorFunctions {
 
 	private static List<AttributeCalculatorFunction> functions;
-
+	
 	private AttributeCalculatorFunctions() {
 
 	}
@@ -29,11 +29,57 @@ public class AttributeCalculatorFunctions {
 	 */
 	public static AttributeCalculatorFunction get(int functionIndex) {
 		if (functions == null) {
-			functions = new ArrayList<AttributeCalculatorFunction>();
+			initialize();
 		}
 
 		return functions.get(functionIndex);
 
 	}
 
+	private static void initialize() {
+		
+		functions = new ArrayList<AttributeCalculatorFunction>();
+		
+		AttributeCalculatorFunction function;
+
+		// !!! the order of creation and adding must be conform to the order of enumeration AttributeCalculatorFunctionTableType
+		// because the function array index has to match to the int value that belongs to the AttributeCalculatorFunctionTableType
+
+		// add identical function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.identical);
+		functions.add(function);
+
+		// add positive_raise function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.positive_raise);
+		functions.add(function);
+
+		// add negative_raise function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.negative_raise);
+		functions.add(function);
+
+		// add v function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.v);
+		functions.add(function);
+		
+		// add v_mirrored function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.v_mirrored);
+		functions.add(function);
+
+		// add u function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.u);
+		functions.add(function);
+		
+		// add u_mirrored function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.u_mirrored);
+		functions.add(function);
+
+		// add horizontal_min function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.horizontal_min);
+		functions.add(function);
+		
+		// add horizontal_max function 
+		function = new AttributeCalculatorFunctionTable(AttributeCalculatorFunctionTableType.horizontal_max);
+		functions.add(function);
+		
+	}
 }
