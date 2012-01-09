1fa5d14bf636c738e1c8fdad8d35183940acaa4c
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2012-01-09 23:43:01 +0000

    enum with int index

--- trunk/org.socialworld/src/org/socialworld/calculation/AttributeCalculatorFunctionTableType.java
+++ trunk/org.socialworld/src/org/socialworld/calculation/AttributeCalculatorFunctionTableType.java
@@ -1,7 +1,43 @@
 package org.socialworld.calculation;
 
-public enum AttributeCalculatorFunctionTableType {
-	identical, positive_raise, negative_raise, 
-	v, v_mirrored, u, u_mirrored, horizontal_min, horizontal_max
 
+public enum AttributeCalculatorFunctionTableType {
+	identical(0), positive_raise(1), negative_raise(2), 
+	v(3), v_mirrored(4), u(5), u_mirrored(6), horizontal_min(7), horizontal_max(8);
+	
+	/**
+	 * The constant holds the informations how many attributes are simulated.
+	 * The constant is used for iteration about all attributes and for creation
+	 * of attribute arrays.
+	 */
+	public static final int NUMBER_OF_AFCT_TYPES = 9;
+
+	private int arrayIndex;
+
+	private AttributeCalculatorFunctionTableType(int index) {
+		this.arrayIndex = index;
+	}
+
+	/**
+	 * The method returns the index of the attribute calculator function table.
+	 * 
+	 * @return attribute's index
+	 */
+	public int getIndex() {
+		return arrayIndex;
+	}
+	
+	/**
+	 * The method returns the acft type which belongs to the parameter index.
+	 * 
+	 * @param arrayIndex
+	 *            attribute index
+	 * @return attribute name
+	 */
+	public AttributeCalculatorFunctionTableType getType(int arrayIndex) {
+		for (AttributeCalculatorFunctionTableType type : AttributeCalculatorFunctionTableType.values())
+			if (type.arrayIndex == arrayIndex)
+				return type;
+		return null;
+	}
 }
