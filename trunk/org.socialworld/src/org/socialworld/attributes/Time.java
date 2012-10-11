88f2e0321622a750cda6859c0497c9e1ae5ef177
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2012-10-11 20:00:21 +0000

    2 constructors implemented

--- trunk/org.socialworld/src/org/socialworld/attributes/Time.java
+++ trunk/org.socialworld/src/org/socialworld/attributes/Time.java
@@ -11,7 +11,49 @@ package org.socialworld.attributes;
  * @author Mathias Sikos (tyloesand) 
  */
 public class Time {
+	
+	int days;
+	byte hours;
+	byte minutes;
+	byte seconds;
+	short milliseconds;
+	
+	long totalMilliseconds;
+	
 	public Time() {
-
+		this.totalMilliseconds = now();
+		
+		separate();
 	}
+
+	public Time(long totalmilliseconds) {
+		this.totalMilliseconds = totalmilliseconds;
+		separate();
+	}
+
+	private void separate() {
+		long rest;
+		
+		rest = this.totalMilliseconds;
+		
+		this.milliseconds = (short) (rest % 1000);
+		rest = (long) (rest / 1000);
+		
+		this.seconds = (byte) (rest % 60);
+		rest = (long) (rest / 60);
+
+		this.minutes = (byte) (rest % 60);
+		rest = (long) (rest / 60);
+
+		this.hours = (byte) (rest % 24);
+		rest = (long) (rest / 24);
+
+		this.days = (int) rest;
+		
+	}
+	
+	private long now() { 
+		return 0;
+	}
+
 }
