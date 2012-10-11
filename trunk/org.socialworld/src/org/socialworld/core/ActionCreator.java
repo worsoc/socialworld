be859ef91d2dfff4cdf20e0440aec8578c271fbf
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2012-10-11 20:00:59 +0000

    duration is type long now

--- trunk/org.socialworld/src/org/socialworld/core/ActionCreator.java
+++ trunk/org.socialworld/src/org/socialworld/core/ActionCreator.java
@@ -1,13 +1,21 @@
 package org.socialworld.core;
 
 import org.socialworld.objects.SimulationObject;
+import org.socialworld.objects.Animal;
+import org.socialworld.attributes.AttributeArray;
+import org.socialworld.attributes.ActionMode;
+import org.socialworld.attributes.ActionType;
+import org.socialworld.attributes.Direction;
+import org.socialworld.attributes.Time;
 import org.socialworld.calculation.EventReactionAssignment;
 import org.socialworld.calculation.EventReactionDescription;
+import org.socialworld.calculation.Vector;
 
 public class ActionCreator extends Semaphore {
 
 	private static ActionCreator creator;
-
+	Action action;
+	
 	/**
 	 * Because of being a singleton the instance is created in a private
 	 * constructor.
@@ -29,21 +37,13 @@ public class ActionCreator extends Semaphore {
 	}
 
 	public SemaphoreReturnCode createAction(
-			SimulationObject actor,
+			Animal actor,
 			Event event,
 			Object user) {
 		
 		if (this.locker == user) {
-			EventReactionDescription eventReactionDescription;
-			int eventType;
-			int eventReactionType;
 			
-			eventType = event.getEventType();
-			eventReactionType = actor.getReactionType(eventType);
-
-			eventReactionDescription = 
-				EventReactionAssignment.getInstance().getEventReactionDescription(
-					eventType, eventReactionType	);
+			createReaction(event, actor);
 			
 			return SemaphoreReturnCode.success;
 		}
@@ -53,5 +53,59 @@ public class ActionCreator extends Semaphore {
 		
 		
 
+	}
+	
+	private void createReaction(Event event,
+			Animal actor) {
+		EventReactionDescription eventReactionDescription;
+		int eventType;
+		int eventReactionType;
+		
+		AttributeArray attributeArray;
+		
+		ActionType actionType;
+		ActionMode actionMode;
+		SimulationObject target;
+		Direction direction;
+		double intensity;
+		Time minTime;
+		Time maxTime;
+		int priority;
+		long duration;
+		
+		Vector relativeDirection;
+		
+		eventType = event.getEventType();
+		eventReactionType = actor.getReactionType(eventType);
+
+		eventReactionDescription = 
+			EventReactionAssignment.getInstance().getEventReactionDescription(
+				eventType, eventReactionType	);
+		
+		attributeArray = actor.getAttributes();
+		
+		actionType = eventReactionDescription.getActionTypeExpression().evaluateExpression(
+						attributeArray);
+		actionMode = eventReactionDescription.getActionModeExpression().evaluateExpression(
+				attributeArray);
+		relativeDirection = eventReactionDescription.getRelativeDirectionExpression().evaluateExpression(
+				attributeArray);
+		intensity = eventReactionDescription.getIntensityExpression().evaluateExpression(
+				attributeArray );
+		priority = eventReactionDescription.getPriorityExpression().evaluateExpression(
+				attributeArray );
+		duration = eventReactionDescription.getDurationExpression().evaluateExpression(
+				attributeArray );
+		
+		direction = event.getDirection();
+		direction.multiply(relativeDirection);
+		
+		target = event.getCauser();
+		
+		minTime = new Time();
+		maxTime = new Time(duration);
+		
+		action = new Action(actionType , actionMode , target,  direction,
+				 intensity,  minTime,  maxTime,	 priority,  duration);
 	}
 }
