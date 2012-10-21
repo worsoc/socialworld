11d8304248506934f75dad0afd6ec6fbab3aa49b
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2012-10-21 15:28:12 +0000

    creator classes have write access to simulation objects, of course

---
+++ org.socialworld/src/org/socialworld/datasource/LoadAnimal.java
@@ -0,0 +1,16 @@
+package org.socialworld.datasource;
+
+import org.socialworld.objects.Animal;
+import org.socialworld.objects.IAnimalWrite;
+
+public class LoadAnimal extends LoadSimulationObjects implements IAnimalWrite {
+
+	@Override
+	public Animal getObject(long objectID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	
+}
--- org.socialworld/src/org/socialworld/datasource/LoadSimulationObjects.java
+++ org.socialworld/src/org/socialworld/datasource/LoadSimulationObjects.java
@@ -4,16 +4,17 @@
 package org.socialworld.datasource;
 
 import org.apache.log4j.Logger;
-import org.socialworld.objects.*;
+import org.socialworld.objects.ISimulationObjectWrite;
+import org.socialworld.objects.SimulationObject;
 
 /**
- * Its the abstract class for creating simulation objects.
+ * Its the basic class for creating simulation objects.
  * for inherited classes of class SimulationObject
  *  there exists an inherited class of LoadSimulationObjects.
  *  
  * @author Mathias Sikos (tyloesand) 
  */
-public abstract class LoadSimulationObjects {
+public abstract class LoadSimulationObjects implements ISimulationObjectWrite {
 	
 	protected static final Logger logger = Logger.getLogger(LoadSimulationObjects.class);
 
@@ -22,5 +23,7 @@ public abstract class LoadSimulationObjects {
 
 	}
 
-	public abstract SimulationObject getObject(long objectID);
+	public abstract SimulationObject getObject(long objectID) ;
+	
+
 }
