4c2835781ed5ed22eda870751604a0c7c66c0de7
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2012-10-24 20:31:55 +0000

    ActionEventMapping created

---
+++ org.socialworld/src/org/socialworld/calculation/ActionEventMapping.java
@@ -0,0 +1,100 @@
+package org.socialworld.calculation;
+
+import org.socialworld.attributes.ActionMode;
+import org.socialworld.attributes.ActionType;
+import org.socialworld.attributes.Direction;
+import org.socialworld.attributes.Position;
+import org.socialworld.attributes.Time;
+import org.socialworld.core.Action;
+import org.socialworld.core.Event;
+import org.socialworld.objects.SimulationObject;
+
+public class ActionEventMapping {
+
+	private static ActionEventMapping mapping;
+	
+	private ActionEventMapping() {
+		
+	}
+	
+	/**
+	 * The method gets back the only instance of the ActionEventMapping.
+	 * 
+	 * @return singleton object of ActionEventMapping
+	 */
+	public static ActionEventMapping getInstance() {
+		if (mapping == null) mapping = new ActionEventMapping();
+		return mapping;
+	}
+	
+	public Event createEvent(final Action action,	final SimulationObject actor) {
+		// TODO (tyloesand) implement the action event mapping 
+		// and the event property calculation (in private methods, see below)
+		ActionType actionType;
+		ActionMode actionMode;
+		
+		Event event;
+		
+		int eventType;
+		SimulationObject causer;
+		Direction direction;
+		int strength;
+		Time time;
+		int priority;
+		Position position;
+		float effectDistance;
+		float effectAngle;		
+		
+		actionType = action.getType();
+		actionMode = action.getMode();
+				
+		eventType = mapActionToEventType(actionMode, actionType );
+		strength = mapActionToIntensity(actionMode, actionType, action.getIntensity(), actor);
+		priority = mapActionToPriority(actionMode, actionType, action.getPriority());
+		position = calculateEventPosition(action, actor);
+		direction = calculateEventDirection(action, actor);
+		causer = actor;
+		time = new Time();
+		effectDistance = calculateEffectDistance(action, actor);
+		effectAngle = calculateEffectAngle(action, actor);
+		
+		event = new Event( eventType,  priority,  causer,  time,  position,
+				 direction,  strength, 
+				 effectDistance,  effectAngle);
+
+		return event;
+	}
+	
+	private int mapActionToEventType(ActionMode actionMode, ActionType actionType ) {
+		return 0;
+	}
+	
+	private int mapActionToIntensity(
+			ActionMode actionMode, ActionType actionType, double intensity, SimulationObject actor) {
+		return (int) intensity;
+	}
+	
+	private int mapActionToPriority(ActionMode actionMode, ActionType actionType, int priority ) {
+		return priority;
+	}
+
+	private Position calculateEventPosition(final Action action,	final SimulationObject actor ) {
+		// getPosition() returns a copy of actor's position!
+		Position eventposition = actor.getPosition();
+		return eventposition;
+	}
+
+	private Direction calculateEventDirection(final Action action,	final SimulationObject actor ) {
+		Direction eventDirection = new Direction(action.getDirection());
+		return eventDirection;
+	}
+
+	private float calculateEffectDistance(final Action action,	final SimulationObject actor ) {
+		return 123;
+	}
+
+	private float calculateEffectAngle(final Action action,	final SimulationObject actor ) {
+		return 12;
+	}
+
+}
