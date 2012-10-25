309ef18c0554b521cc2482f1bf34ab350eb3d814
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2012-10-25 20:30:41 +0000

    no Semaphore anymore

--- trunk/org.socialworld/src/org/socialworld/core/EventCreator.java
+++ trunk/org.socialworld/src/org/socialworld/core/EventCreator.java
@@ -3,66 +3,28 @@ package org.socialworld.core;
 import org.socialworld.objects.SimulationObject;
 import org.socialworld.calculation.ActionEventMapping;
 
-public class EventCreator extends Semaphore {
+public class EventCreator  {
 
-	private static EventCreator creator;
 	private ActionEventMapping mapping;
 	private Event event;
 	
-	/**
-	 * Because of being a singleton the instance is created in a private
-	 * constructor.
-	 */
-	private EventCreator() {
-		locker = null;
+	public EventCreator() {
 		mapping = ActionEventMapping.getInstance();
 	}
 	
-	/**
-	 * The method gets back the only instance of the EventCreator.
-	 * 
-	 * @return singleton object of EventCreator
-	 */
-	public static EventCreator getInstance() {
-		if (creator == null) {
-			creator = new EventCreator();
-		}
-		return creator;
-	}
 
 	/**
-	 * If the caller is the owner of the semaphore (that means the owner of the event object),
-	 * the method returns the last created event object.
-	 * In other cases it returns null.
-	 * 
-	 * It is assumed, that the caller locks the semaphore 
-	 * and that the method createEvent() has been called before the method getEvent() is called().
-	 */
-	public Event getEvent(final Object user) {
-		if (this.locker == user) 
-			return this.event;
-		else
-			return null;
-	}	
-	/**
 	 * The method creates a new event for an action.
-	 * The creation is saved by semaphore.
-	 *  So only the object, that locked the event creator, can create the event object.	  
 	 */
-	public SemaphoreReturnCode createEvent(
+	public Event createEvent(
 			final SimulationObject actor,
-			final Action action,
-			final Object user) {
+			final Action action) {
 		
-		if (this.locker == user) {
+			this.event = null;
 			
 			createEvent(action, actor);
 			
-			return SemaphoreReturnCode.success;
-		}
-		if (this.locker == null)
-			return SemaphoreReturnCode.notLockedByAnyone;
-		return SemaphoreReturnCode.lockedByAnotherUser;
+			return this.event;
 	}
 	
 	/**
