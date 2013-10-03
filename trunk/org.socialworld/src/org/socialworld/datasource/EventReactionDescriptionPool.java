2e2186df572af4162118cac9b92b68dddd01fbdc
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2013-10-03 19:36:05 +0000

    private method createDescription() deleted

--- trunk/org.socialworld/src/org/socialworld/datasource/EventReactionDescriptionPool.java
+++ trunk/org.socialworld/src/org/socialworld/datasource/EventReactionDescriptionPool.java
@@ -39,29 +39,12 @@ public class EventReactionDescriptionPool {
 	
 	public EventReactionDescription getDescription(int eventType,	int reactionType) {
 		int index;
-		int countFill;
-		int maxFill;
 
-		EventReactionDescription description;
+		EventReactionDescription description = null;
 		
 		index = eventType * Event.MAX_EVENT_TYPE + reactionType;
 		
-		if (descriptions.size() >= index) {
-			description = descriptions.get(index);
-			if (description == null) {
-				description = createDescription(eventType, reactionType);
-				descriptions.set(index, description);
-			}
-		}	
-		else {
-			maxFill = index - descriptions.size();
-			for ( countFill=1; countFill <= maxFill; countFill++) {
-				description = null;
-				descriptions.add(description);
-			}
-			description = createDescription(eventType, reactionType);
-			descriptions.add(description);
-		}
+		if (descriptions.size() >= index) 			description = descriptions.get(index);
 		return description;
 	}
 
@@ -371,10 +354,5 @@ public class EventReactionDescriptionPool {
 		expressions.set(ID - 1, expression);
 	}
 	
-	private EventReactionDescription createDescription(int eventType,	int reactionType) {
-		EventReactionDescription description = new	EventReactionDescription();
-		
-		return description;
-	}
 
 }
