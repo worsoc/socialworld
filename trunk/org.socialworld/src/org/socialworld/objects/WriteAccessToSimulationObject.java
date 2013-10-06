9465ee9964e8f7ab6120992e61dc8fe7f06a4b6f
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2013-10-06 18:06:32 +0000

    set methods for types

--- trunk/org.socialworld/src/org/socialworld/objects/WriteAccessToSimulationObject.java
+++ trunk/org.socialworld/src/org/socialworld/objects/WriteAccessToSimulationObject.java
@@ -26,4 +26,15 @@ public class WriteAccessToSimulationObject {
 		if (caller instanceof ISimulationObjectWrite) object.setAction(action, this);
 	}
 
+	public void setInfluenceTypes(int types[], Object caller) {
+		if (caller instanceof ISimulationObjectWrite) object.setInfluenceTypes(types, this);
+	}
+
+	public void setReactionTypes(int types[], Object caller) {
+		if (caller instanceof ISimulationObjectWrite) object.setReactionTypes(types, this);
+	}
+
+	public void setState2ActionType(int type, Object caller) {
+		if (caller instanceof ISimulationObjectWrite) object.setState2ActionType(type, this);
+	}
 }
