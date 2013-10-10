0545e0816c10166aa10e3c81d032e6f683aa7c49
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2013-10-10 19:59:40 +0000

    initObject() for initilializing a new created object

--- org.socialworld/src/org/socialworld/datasource/LoadAnimal.java
+++ org.socialworld/src/org/socialworld/datasource/LoadAnimal.java
@@ -2,15 +2,43 @@ package org.socialworld.datasource;
 
 import org.socialworld.objects.Animal;
 import org.socialworld.objects.IAnimalWrite;
+import org.socialworld.objects.WriteAccessToAnimal;
 
 public class LoadAnimal extends LoadSimulationObjects implements IAnimalWrite {
 
-	@Override
-	public Animal getObject(long objectID) {
-		// TODO implement how to get an animal object by objectID
-		return null;
+	protected LoadAnimal() {
 	}
 
+	@Override
+	public Animal getObject(long objectID) {
+		double gauss_value;
+		
+		Animal createdAnimal = new Animal();
+		WriteAccessToAnimal animal = new WriteAccessToAnimal(createdAnimal);
+		
+		gauss_value = random.nextGaussian();
 
+		initObject(animal, objectID, gauss_value);	
+		
+		return createdAnimal;
+	}
+
+	protected void initObject(WriteAccessToAnimal object, long objectID, double gauss_value) {
+		int indexACMP;
+		int indexAAP;
+
+		super.initObject(object, objectID, gauss_value);
+
+		indexACMP = mapGaussToIndex(gauss_value, AttributeCalculatorMatrixPool.CAPACITY_ACMP_ARRAY);
+		indexAAP = mapGaussToIndex(gauss_value, AttributeArrayPool.CAPACITY_AAP_ARRAY);
+
+		object.setMatrix(	
+				AttributeCalculatorMatrixPool.getInstance().getMatrix(indexACMP),
+				this);
+		object.setAttributes(
+				AttributeArrayPool.getInstance().getArray(indexAAP),
+				this);
+		
+	}
 	
 }
