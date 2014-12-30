9f149470e3f0fe1a48bd605719d0460560c48673
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2014-12-30 13:28:25 +0000

    methods boolean equals(Position b) and Vector getDirectionTo(Position b)

--- trunk/org.socialworld/src/org/socialworld/attributes/Position.java
+++ trunk/org.socialworld/src/org/socialworld/attributes/Position.java
@@ -31,6 +31,8 @@ import org.socialworld.calculation.Vector;
 public class Position {
 
 	public final int LOCATIONBYBASEMAXLENGTH = 9;
+	public static final int LOCATIONBASE9 = 9;
+	public static final int LOCATIONBASE25 = 25;
 	
 	private Vector m_position;
 	
@@ -75,23 +77,52 @@ public class Position {
 		Vector direction = m_position.getDirectionFrom(position.getVector());
 		return direction;
 	}
-	
+
+	/**
+	 * The method calculates the direction between two positions. The result
+	 * vector has the direction from this position to parameter position.
+	 * 
+	 * @param position
+	 * @return the direction
+	 */
+	public Vector getDirectionTo(Position position) {
+		Vector direction = position.getVector().getDirectionFrom(m_position);
+		return direction;
+	}
+
 	public int getX() { return (int) m_position.getX(); }
 	public int getY() { return (int) m_position.getY(); }
 	public int getZ() { return (int) m_position.getZ(); }
 	
+	public boolean equals(Position b, int locationBase, int accuracyLevel) {
+		if (locationBase == LOCATIONBASE25)  {
+			return (locationByBase25.substring(0, accuracyLevel - 1).
+					compareTo(b.getLocationByBase25().substring(0, accuracyLevel - 1)) == 0 );
+		}
+		if (locationBase == LOCATIONBASE9)  {
+			return (Integer.toString(locationByBase9).substring(0, accuracyLevel - 1).
+					compareTo(Integer.toString(b.getLocationByBase9()).substring(0, accuracyLevel - 1)) == 0 );
+		}
+
+		return equals(b);
+	}
+	
+	public boolean equals(Position b) {
+		return m_position.equals(b.getVector());
+	}
 	
 	private void setLocationByBases(Vector position)
 	{
-		this.locationByBase25 = getLocationByBase(position, 5);
+		this.locationByBase25 = getLocationByBase(position, LOCATIONBASE25);
 		
-		String locationByBase9 = getLocationByBase(position, 3);
+		String locationByBase9 = getLocationByBase(position, LOCATIONBASE9);
 		this.locationByBase9 = Integer.parseInt( locationByBase9 ); 
 	}
 
-	private String getLocationByBase(Vector position, int baseSquareRoot)
+	private String getLocationByBase(Vector position, int base)
 	{ 
 		String locationString = "";
+		int baseSquareRoot;
 		
 		int i;
 		int sectorX;
@@ -102,6 +133,8 @@ public class Position {
 		
 		float x = position.getX();
 		float y = position.getY();
+		
+		baseSquareRoot = (int) Math.sqrt(base);
 		
 		for (i = 0; i < LOCATIONBYBASEMAXLENGTH; i++) {
 			
@@ -116,18 +149,18 @@ public class Position {
 			
 			sector = sectorY * baseSquareRoot + sectorX;
 			
-			locationString = locationString + getSectorCode(sector, baseSquareRoot) ;
+			locationString = locationString + getSectorCode(sector, base) ;
 		}
 		
 		return locationString;
 		
 	}
 	
-	private String getSectorCode(int sector, int baseSquareRoot ) {
+	private String getSectorCode(int sector, int base ) {
 		
-		if (baseSquareRoot == 3) return Integer.toString(sector);
+		if (base == LOCATIONBASE9) return Integer.toString(sector);
 		
-		if (baseSquareRoot == 5) 
+		if (base == LOCATIONBASE25) 
 			// 64 + 1 --> "A", ... , 64 + 25 --> "Y"
 			return String.valueOf(Character.toChars(64 + sector  ));
 		else
