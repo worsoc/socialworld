5a93ed23465ea5c644bf41a7d5e0ec14718a0722
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2015-01-09 20:40:55 +0000

    type anser added

--- org.socialworld/src/org/socialworld/calculation/Type.java
+++ org.socialworld/src/org/socialworld/calculation/Type.java
@@ -22,7 +22,7 @@
 package org.socialworld.calculation;
 
 public enum Type {
-	integer, longinteger, floatingpoint, vector, bool, nothing, actionType,
-	attributeArray, event, action, simulationObject
+	integer, longinteger, floatingpoint, string, vector, bool, nothing, actionType,
+	attributeArray, event, action, simulationObject, answer
 
 }
