9a223758b6c9efc6baa9f4d61672bee8aa43ba7f
tyloesand <tyloesand@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2016-07-18 18:51:45 +0000

    class Program added

---
+++ trunk/org.socialworld/src/org/socialworld/calculation/expressions/Program.java
@@ -0,0 +1,11 @@
+package org.socialworld.calculation.expressions;
+
+import org.socialworld.calculation.Expression;
+
+public class Program extends Expression {
+
+	public Program(Expression startExpression) {
+		
+		setExpression1(startExpression);
+	}
+}
