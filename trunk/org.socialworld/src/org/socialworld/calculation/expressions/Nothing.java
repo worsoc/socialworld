934dcb0e20b66d33cf96197de38593eb1f193ed1
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2019-08-14 21:20:41 +0000

    Nothing as Singleton

--- trunk/org.socialworld/src/org/socialworld/calculation/expressions/Nothing.java
+++ trunk/org.socialworld/src/org/socialworld/calculation/expressions/Nothing.java
@@ -4,8 +4,19 @@ import org.socialworld.calculation.Expression;
 
 public class Nothing extends Expression {
 
-	public Nothing() {
+	private static Nothing instance;
+	
+	private Nothing() {
 		super();
 		setValid();
 	}
+	
+	public static Nothing getInstance() {
+		if (instance == null) {
+			instance = new Nothing();
+		}
+		return instance;
+		
+	}
+
 }
