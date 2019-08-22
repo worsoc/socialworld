43dc3a6d7a1dc8255978e380a856f8a7f3746043
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2019-08-22 21:11:59 +0000

    creating value from CreateActionExpression to CreateValue

--- org.socialworld/src/org/socialworld/calculation/expressions/CreateActionExpression.java
+++ org.socialworld/src/org/socialworld/calculation/expressions/CreateActionExpression.java
@@ -21,15 +21,12 @@
 */
 package org.socialworld.calculation.expressions;
 
-import org.socialworld.actions.ActionMode;
-import org.socialworld.actions.ActionType;
 import org.socialworld.attributes.Attribute;
-import org.socialworld.attributes.Time;
 import org.socialworld.calculation.Expression;
 import org.socialworld.calculation.Expression_ConditionOperator;
+import org.socialworld.calculation.Expression_Function;
 import org.socialworld.calculation.Type;
 import org.socialworld.calculation.Value;
-import org.socialworld.calculation.application.ActionCreator;
 import org.socialworld.datasource.parsing.ParseExpressionStrings;
 
 public class CreateActionExpression extends Expression {
@@ -54,52 +51,22 @@ public class CreateActionExpression extends Expression {
 				exp3 = parseLinesTail(1, lines);
 			}
 			else {
-				exp3 = new Expression();
+				exp3 = new CreateValue(Type.action, Nothing.getInstance());
+
 			}
 			
 			setExpression1(exp1);
 			setExpression2(exp2);
 			setExpression3(exp3);
-	
+
+			setOperation(Expression_Function.branching);
+
 			setValid();
 		}
 		
 	}
 
-	protected Value createValue(Type valueType, Value[] arguments) {
 
-		Object createdObject;
-		Value createdValue;
-		
-		Value localArguments[] = new Value[8];
-		
-		// copy attribute array to local argument array
-		localArguments[0] = calculation.copy(arguments[0]);
-		localArguments[1] = new Value(Type.actionType, "actiontype", ActionType.ignore);  
-		localArguments[2] = new Value(Type.actionMode, "actionmode", ActionMode.ignore);  
-		localArguments[3] = new Value(Type.floatingpoint, "intensity", 0);  
-		localArguments[4] = new Value(Type.time, "mintime", 0);  
-		localArguments[5] = new Value(Type.time, "maxtime", 0);  
-		localArguments[6] = new Value(Type.integer, "priority", 0);  
-		localArguments[7] = new Value(Type.longinteger, "duration", 0); 
-		
-		evaluateExpression1(localArguments);
-		
-		ActionType type = (ActionType) localArguments[1].getValueCopy();
-		ActionMode mode = (ActionMode) localArguments[2].getValueCopy();
-		float intensity = (float) localArguments[3].getValueCopy();
-		Time minTime = (Time) localArguments[4].getValueCopy();
-		Time maxTime = (Time) localArguments[5].getValueCopy();
-		int priority = (int) localArguments[6].getValueCopy();
-		long duration = (long) localArguments[7].getValueCopy();
-		
-		createdObject = ActionCreator.createAction(type, mode, intensity, minTime, maxTime, priority, duration) ;
-		createdValue = calculation.createValue(valueType, createdObject);
-		
-		return createdValue;
-		
-	}
-	
 	
 	private Expression parseLinesTail(int index, String[] lines) {
 		
@@ -141,7 +108,7 @@ public class CreateActionExpression extends Expression {
 	
 			for (int i = 0; i < countORs; i++) {
 				
-				disjunctionParts[0] = parseConjunction(listORs[i]);
+				disjunctionParts[i] = parseConjunction(listORs[i]);
 				
 			}
 			
