9cfa2c0d74dca06d727ca51faf38fa119d30944a
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2019-10-14 19:17:21 +0000

    initializeWithTestData_FunctionByExpression() uses List<String> instead of String[]

--- trunk/org.socialworld/src/org/socialworld/datasource/pool/EventInfluenceDescriptionPool.java
+++ trunk/org.socialworld/src/org/socialworld/datasource/pool/EventInfluenceDescriptionPool.java
@@ -1,22 +1,55 @@
+/*
+* Social World
+* Copyright (C) 2019  Mathias Sikos
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.  
+*
+* or see http://www.gnu.org/licenses/gpl-2.0.html
+*
+*/
 package org.socialworld.datasource.pool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.socialworld.calculation.Expression;
+import org.socialworld.calculation.FunctionByExpression;
 import org.socialworld.calculation.descriptions.EventInfluenceDescription;
+import org.socialworld.calculation.expressions.ChangeAttributes;
+import org.socialworld.calculation.expressions.Nothing;
 import org.socialworld.calculation.expressions.Sequence;
 import org.socialworld.core.EventType;
 import org.socialworld.datasource.tablesPool.TablePoolEID;
 
 public class EventInfluenceDescriptionPool extends DescriptionPool {
 
+	public static final int COUNT_FbE_TEST_ENTRIES = 2;		// Anzahl Testeintraege FunctionByExpression
+	
 	private static EventInfluenceDescriptionPool instance;
 	
 	private static EventInfluenceDescription descriptions[];
+	private FunctionByExpression expressions[];
 	
 	private EventInfluenceDescriptionPool () {
 		
 		sizeDescriptionsArray = EventType.MAX_EVENT_TYPE * GaussPoolInfluenceType.CAPACITY_GPIT_ARRAY;
 		descriptions = new EventInfluenceDescription[sizeDescriptionsArray];
-		
+
+		expressions = new FunctionByExpression[COUNT_FbE_TEST_ENTRIES];
+
+		initializeWithTestData_FunctionByExpression();
 		
 		initialize();
 	}
@@ -54,14 +87,52 @@ public class EventInfluenceDescriptionPool extends DescriptionPool {
 		
 	}
 
-	protected void initialize() {
-		// initialize with  dummy descriptions with an expression that returns the invalid "nothing" value
-		for (int index = 0; index < sizeDescriptionsArray; index++)
-			descriptions[index] = new EventInfluenceDescription();
+	
+	private void initializeWithTestData_FunctionByExpression() {
 		
-		loadFromDB();
+		int 		expressionsCount = COUNT_FbE_TEST_ENTRIES;
+		List<List<String>> expressions = new ArrayList<List<String>>(expressionsCount);
+		List<String> lines;
+		Expression startExpression = Nothing.getInstance();
+
+		lines = new ArrayList<String>(2);
+		lines.add("WENN mood > 45 & mood < 52 DANN <MOOD><MX+N>0;1.2;23</MX+N></MOOD><COURAGE><MX+1>1;1;1</MX+N></COURAGE>");
+		lines.add("WENN mood > 70 & mood < 85 DANN <MOOD><MX+N>0;0.9;-10</MX+N></MOOD><COURAGE><MX+1>1;1;-1</MX+N></COURAGE>");
+		expressions.add(lines);
+		
+		lines = new ArrayList<String>(1);
+		lines.add("WENN power > 80 & morals > 60 DANN <POWER><MX+N>8;1;-1</MX+N></POWER><MORALS><MX+1>2;1;-1</MX+N></MORALS>");
+		expressions.add(lines);
+
+		for (int i = 0; i <  expressionsCount; i++) {
+			startExpression = new ChangeAttributes(expressions.get(i));
+			this.expressions[i] = new FunctionByExpression(startExpression);
+
+		}
+		
+	}
+	
+	
+	protected void initialize() {
+
+		EventInfluenceDescription description;
+		int indexExps;
+		int linesCount = COUNT_FbE_TEST_ENTRIES;
+		
+		for (int index = 0; index < sizeDescriptionsArray; index++) {
+			
+			description = new EventInfluenceDescription();
+			for (indexExps = 0; indexExps <  linesCount; indexExps++) {
+				description.addFunctionEventInfluence(expressions[indexExps]);
+			}
+			descriptions[index] = description;
+	
+		}
+		
+		//loadFromDB();
 	}
 
+	
 	private void loadFromDB() {
 		TablePoolEID tableEID;
 
