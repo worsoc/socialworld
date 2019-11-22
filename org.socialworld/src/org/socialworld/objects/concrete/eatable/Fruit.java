a5a2b00d50572f7eee9739cc0a92f1ead2ef7ca5
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2019-11-22 21:24:24 +0000

    structure for (more) concrete simulation object classes

---
+++ org.socialworld/src/org/socialworld/objects/concrete/eatable/Fruit.java
@@ -0,0 +1,12 @@
+package org.socialworld.objects.concrete.eatable;
+
+import org.socialworld.objects.Item;
+
+public abstract class Fruit extends Item  {
+
+	public Fruit(int objectID) {
+		super(objectID);
+	}
+	
+
+}
---
+++ org.socialworld/src/org/socialworld/objects/concrete/eatable/fruits/Apple.java
@@ -0,0 +1,31 @@
+package org.socialworld.objects.concrete.eatable.fruits;
+
+import org.socialworld.attributes.properties.NutrientProperty;
+import org.socialworld.attributes.properties.TasteProperty;
+import org.socialworld.objects.concrete.eatable.Fruit;
+import org.socialworld.objects.properties.IEatable;
+import org.socialworld.objects.properties.IThrowable;
+
+public class Apple extends Fruit implements IEatable, IThrowable {
+
+	NutrientProperty nutrientProps;
+	TasteProperty tasteProps;
+	
+	public Apple(int objectID) {
+		super(objectID);
+	}
+	
+	public  float getTemperature() {return 20; };
+	public  float getConsistence() {return 20; };
+	public  float getFirmness() {return 20; };
+	
+	public  NutrientProperty getNutrientProperties() { return nutrientProps; }
+	
+	public  TasteProperty getTasteProperties() { return tasteProps; }
+
+	public float getGrip() { return 20; }
+	public float getMass()  { return 20; }
+	public float getVolume()  { return 20; }
+	public float getForm()  { return 20; }
+
+}
