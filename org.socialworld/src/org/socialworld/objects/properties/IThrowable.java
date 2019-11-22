e7a5b10212f3043b7d1420a4d6d47b90087c86e9
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2019-11-22 21:26:09 +0000

    more interfaces for item properties

---
+++ org.socialworld/src/org/socialworld/objects/properties/IThrowable.java
@@ -0,0 +1,10 @@
+package org.socialworld.objects.properties;
+
+public interface IThrowable {
+
+	public abstract float getGrip();
+	public abstract float getMass();
+	public abstract float getVolume();
+	public abstract float getForm();
+	
+}
