7b8785fbab46947ff1f26597ed0fa9f6406cef00
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2019-12-16 21:43:48 +0000

    performance creating rectangles in Visibility constructor for cuboids with standard perpendiculars

--- org.socialworld/src/org/socialworld/attributes/percipience/Visibility.java
+++ org.socialworld/src/org/socialworld/attributes/percipience/Visibility.java
@@ -28,24 +28,29 @@ public class Visibility {
 		
 	}
 
-	public Visibility(Position middlePointTotal, float[] abcQuaderWithStandardPerpendiculars) {
+	public Visibility(Position middlePointTotal, float[] abcCuboidWithStandardPerpendiculars) {
 		
 		Rectangle visibilityRectangles[];
 		visibilityRectangles = new Rectangle[VectorMapper.COUNT_STANDARD_VISIBLE_AREA_PERPENDICULARS];
 		
 		
-		if (abcQuaderWithStandardPerpendiculars.length == 3) {
+		if (abcCuboidWithStandardPerpendiculars.length == 3) {
 			
 			float a;  // --> x
 			float b;  // --> y
 			float c;  // --> z
 			
+			a = abcCuboidWithStandardPerpendiculars[0];
+			b = abcCuboidWithStandardPerpendiculars[1];
+			c = abcCuboidWithStandardPerpendiculars[2];
+			
 			float g;
 			float h;
-			
-			a = abcQuaderWithStandardPerpendiculars[0];
-			b = abcQuaderWithStandardPerpendiculars[1];
-			c = abcQuaderWithStandardPerpendiculars[2];
+
+			float hypotenuseAB = hypotenuse(a, b);
+			float hypotenuseAC = hypotenuse(a, c);
+			float hypotenuseBC = hypotenuse(b, c);
+			float cuboidDiagonal = lengthVector(a, b, c);
 			
 			for (int nrPerpendicular = 0; nrPerpendicular < VectorMapper.COUNT_STANDARD_VISIBLE_AREA_PERPENDICULARS; nrPerpendicular++) {
 				
@@ -63,45 +68,45 @@ public class Visibility {
 				case 5:
 					g = a; h = b; break; 	// g = -a 
 				case 6:
-					g = hypotenuse(a, b); h = c; break; // g = hypotenuse(-a, b)
+					g = hypotenuseAB; h = c; break; // g = hypotenuse(-a, b)
 				case 7:
-					g = hypotenuse(a, b); h = c; break; // g = hypotenuse(a, -b)
+					g = hypotenuseAB; h = c; break; // g = hypotenuse(a, -b)
 				case 8:
-					g = hypotenuse(a, b); h = c; break; 
+					g = hypotenuseAB; h = c; break; // g = hypotenuse(a, b)
 				case 9:
-					g = hypotenuse(a, b); h = c; break; // g = hypotenuse(-a, -b)
+					g = hypotenuseAB; h = c; break; // g = hypotenuse(-a, -b)
 				case 10:
-					g = b; h = hypotenuse(a, c); break; // h = hypotenuse(-a, c)
+					g = b; h = hypotenuseAC; break; // h = hypotenuse(-a, c)
 				case 11:
-					g = b; h = hypotenuse(a, c); break; // g = -b
+					g = b; h = hypotenuseAC; break; // g = -b, h = hypotenuse(a, c)
 				case 12:
-					g = a; h = hypotenuse(b, c); break; // g = -a;  h = hypotenuse(-b, c)
+					g = a; h = hypotenuseBC; break; // g = -a;  h = hypotenuse(-b, c)
 				case 13:
-					g = a; h = hypotenuse(b, c); break; 
+					g = a; h = hypotenuseBC; break; // h = hypotenuse(b, c)
 				case 14:
-					g = hypotenuse(a, b); h = lengthVector(a, b, c); break;  // g = hypotenuse(-a, b), lengthVector(-a, -b, c)
+					g = hypotenuseAB; h = cuboidDiagonal; break;  // g = hypotenuse(-a, b), h = lengthVector(-a, -b, c)
 				case 15:
-					g = hypotenuse(a, b); h = lengthVector(a, b, c); break;  // lengthVector(-a, b, c)
+					g = hypotenuseAB; h = cuboidDiagonal; break;  // g = hypotenuse(a, b), h = lengthVector(-a, b, c)
 				case 16:
-					g = hypotenuse(a, b); h = lengthVector(a, b, c); break;  // g = hypotenuse(a, -b)
+					g = hypotenuseAB; h = cuboidDiagonal; break;  // g = hypotenuse(a, -b), h = lengthVector(a, b, c)
 				case 17:
-					g = hypotenuse(a, b); h = lengthVector(a, b, c); break;  // g = hypotenuse(-a, -b), lengthVector(a, -b, c)
+					g = hypotenuseAB; h = cuboidDiagonal; break;  // g = hypotenuse(-a, -b), h = lengthVector(a, -b, c)
 				case 18:
-					g = b; h = hypotenuse(a, c); break; 
+					g = b; h = hypotenuseAC; break; // h = hypotenuse(a, c)
 				case 19:
-					g = b; h = hypotenuse(a, c); break; // g = -b; h = hypotenuse(-a, c)
+					g = b; h = hypotenuseAC; break; // g = -b; h = hypotenuse(-a, c)
 				case 20:
-					g = a; h = hypotenuse(b, c); break; // g = -a; 
+					g = a; h = hypotenuseBC; break; // g = -a; h = hypotenuse(b, c)
 				case 21:
-					g = a; h = hypotenuse(b, c); break; // h = hypotenuse(-b, c)
+					g = a; h = hypotenuseBC; break; // h = hypotenuse(-b, c)
 				case 22:
-					g = hypotenuse(a, b); h = lengthVector(a, b, c); break;  // g = hypotenuse(-a, b)
+					g = hypotenuseAB; h = cuboidDiagonal; break;  // g = hypotenuse(-a, b), h = lengthVector(a, b, c)
 				case 23:
-					g = hypotenuse(a, b); h = lengthVector(a, b, c); break;  // lengthVector(a, -b, c)
+					g = hypotenuseAB; h = cuboidDiagonal; break;  // g = hypotenuse(a, b), h = lengthVector(a, -b, c)
 				case 24:
-					g = hypotenuse(a, b); h = lengthVector(a, b, c); break;  // g = hypotenuse(a, -b), lengthVector(-a, -b, c)
+					g = hypotenuseAB; h = cuboidDiagonal; break;  // g = hypotenuse(a, -b), h = lengthVector(-a, -b, c)
 				case 25:
-					g = hypotenuse(a, b); h = lengthVector(a, b, c); break;  // g = hypotenuse(-a, -b), lengthVector(-a, b, c)
+					g = hypotenuseAB; h = cuboidDiagonal; break;  // g = hypotenuse(-a, -b), h = lengthVector(-a, b, c)
 				default:
 					g = 0; h = 0; break;
 				}
