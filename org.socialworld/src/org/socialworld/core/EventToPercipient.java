fe0a24bd4fc12737d5da0dda41dab53544b86ddb
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2019-12-28 21:24:19 +0000

    implements IPerceptible

--- org.socialworld/src/org/socialworld/core/EventToPercipient.java
+++ org.socialworld/src/org/socialworld/core/EventToPercipient.java
@@ -3,23 +3,59 @@ package org.socialworld.core;
 import org.socialworld.actions.ActionPerformer;
 import org.socialworld.attributes.Position;
 import org.socialworld.attributes.Time;
+import org.socialworld.attributes.percipience.Percipience;
+import org.socialworld.objects.Animal;
 import org.socialworld.objects.SimulationObject;
+import org.socialworld.objects.properties.IPerceptible;
 
-public class EventToPercipient extends Event {
+public class EventToPercipient extends Event implements IPerceptible {
 
+	private Percipience percipience;
+	
+
+	
 	/**
 	 * Constructor
 	 */
 	public EventToPercipient(int eventType,  SimulationObject causer, Time time, Position position,	 ActionPerformer performer) {
 		
 		super(eventType,   causer,  time,  position,  performer);
+		this.percipience = new Percipience(1000);
+
 		
 	}
 	
 	public EventToPercipient(EventType eventType,  SimulationObject causer, int priority, Position position) {
 
 		super(eventType,   causer,    priority,	  position);
+		this.percipience = new Percipience(1000);
+
 
 	}
 	
+	public void setPercipience(float maxDistance, float maxSee, float maxHear, float maxSmell, float maxFeel ) {
+		this.percipience  = new Percipience( maxDistance,  maxSee,  maxHear,  maxSmell,  maxFeel );
+	}
+
+	public boolean checkIsEvent_LetMeBePerceived() {
+		return this.getEventType() == EventType.percipientExists;
+	}
+	
+///////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////    implementing IPerceptible     ////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
+
+	public boolean checkIsPossiblePercipient(Animal possiblePercipient) {
+		return this.percipience.checkIsPossibleSeer(possiblePercipient);
+	}
+	
+	
+	public boolean checkChanceToBeSeen(Animal possibleSeer) {
+		return this.percipience.checkChanceToBeSeen(possibleSeer);
+	}
+	
+	public boolean checkIsPossibleSeer(Animal possibleSeer) {
+		return this.percipience.checkIsPossibleSeer(possibleSeer);
+	}
+
 }
