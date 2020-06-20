cdf838edc225fc345729a0037db4c0eb96c59768
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2020-06-20 16:20:10 +0000

    new ISavedValues method checkHasgetPermission(), most ISavedValues methods are implemented final

--- org.socialworld/src/org/socialworld/attributes/SavedValue.java
+++ org.socialworld/src/org/socialworld/attributes/SavedValue.java
@@ -60,7 +60,7 @@ public abstract class SavedValue implements ISavedValues {
 	///////////////////////////////////////////////////////////////////////////////////////////
 
 
-	public boolean hasPropertyProtection() {
+	public final boolean hasPropertyProtection() {
 		
 		if (this.protection == null) {
 			return false;
@@ -74,15 +74,19 @@ public abstract class SavedValue implements ISavedValues {
 		
 	}
 
-	public PropertyProtection getPropertyProtection() {
+	public final PropertyProtection getPropertyProtection() {
 		return new PropertyProtection(this.protection);
 	};
 	
-	public void setPropertyProtection(PropertyProtection propertyProtection) {
+	public final void setPropertyProtection(PropertyProtection propertyProtection) {
 		this.protection = propertyProtection;
 	}
-	
-	public boolean checkHasUseAsPermission(PropertyUsingAs useAsPermission) {
+
+	public final boolean checkHasGetPermission(SimulationCluster cluster) {
+		return this.protection.checkHasGetPermission(cluster);
+	}
+
+	public final boolean checkHasUseAsPermission(PropertyUsingAs useAsPermission) {
 		return this.protection.checkHasUseAsPermission(useAsPermission);
 	}
 
