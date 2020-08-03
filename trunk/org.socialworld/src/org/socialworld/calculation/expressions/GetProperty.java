7526650d721a7964f57d56abf2ad0390c8c8003c
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2020-08-03 15:49:39 +0000

    GetProperty() with argument GetPropertyMode

--- trunk/org.socialworld/src/org/socialworld/calculation/expressions/GetProperty.java
+++ trunk/org.socialworld/src/org/socialworld/calculation/expressions/GetProperty.java
@@ -21,24 +21,47 @@
 */
 package org.socialworld.calculation.expressions;
 
+import org.socialworld.actions.AbstractAction;
+import org.socialworld.attributes.ISavedValues;
 import org.socialworld.attributes.PropertyName;
 import org.socialworld.calculation.Expression;
 import org.socialworld.calculation.Expression_Function;
 import org.socialworld.calculation.SimulationCluster;
 import org.socialworld.calculation.Type;
 import org.socialworld.calculation.Value;
+import org.socialworld.calculation.ValueProperty;
+import org.socialworld.core.Event;
+import org.socialworld.objects.SimulationObject;
+import org.socialworld.objects.StateSimulationObject;
 
 public class GetProperty extends Expression {
 
-	public GetProperty(SimulationCluster cluster, PropertyName simPropName, String propertyName) {
+	
+	
+
+	public GetProperty(SimulationCluster cluster, GetPropertyMode mode, String name, String propertyName) {
 		
 		super();
 		
 		setOperation(Expression_Function.property);
-		setValue(new Value(Type.simPropName, simPropName));
+		
+		switch(mode) {
+		case property:
+			setValue(new Value(Type.simPropName, PropertyName.forString(name)));
+			break;
+		case method:
+			setValue(new Value(Type.simPropName, PropertyName.method));
+			break;
+		case isA:
+			setValue(new Value(Type.simPropName, PropertyName.isA));
+			break;
+		case check:
+			setValue(new Value(Type.simPropName, PropertyName.check));
+			break;
+		}
 		
 		Expression exp1 = new Constant(new Value(Type.integer, cluster.getIndex()));
-		Expression exp2 = new Constant(new Value(Type.string, ""));
+		Expression exp2 = new Constant(new Value(Type.string, name));
 		Expression exp3 = new Constant(new Value(Type.string, propertyName));
 		
 		setExpression1(exp1);
@@ -46,25 +69,95 @@ public class GetProperty extends Expression {
 		setExpression3(exp3);
 		setValid();
 			
+	}
+	
+	protected ValueProperty getProperty(Value valueObject, SimulationCluster cluster, PropertyName simPropName, String methodName, String valueName) {
+		
+		ValueProperty result;
+		result = ValueProperty.getInvalid();
+		Object object = valueObject.getValue();
+		
+		switch (simPropName) {
+		case method:
 			
+			if (methodName.length() > 0) {
+				
+				// use reflection for calling the method
+				
+				if (object instanceof ISavedValues) {
+					ISavedValues savedValue;
+					savedValue = (ISavedValues) object;
+					result = savedValue.getPropertyFromMethod(cluster, methodName, valueName);
+				}
+				
+			}
+			break;
+			
+		case isA:
+			
+			if (methodName.length() > 0) {
+				
+				
+				if (object instanceof SimulationObject) {
+					SimulationObject simObj;
+					simObj = (SimulationObject) object;
+					result = simObj.isA(methodName, valueName);
+				}
+				
+			}
+			break;
+
+		case check:
+			
+			if (methodName.length() > 0) {
+				
+				
+				if (object instanceof SimulationObject) {
+					SimulationObject simObj;
+					simObj = (SimulationObject) object;
+					result = simObj.check(methodName, valueName);
+				}
+				
+			}
+			break;
+			
+		case unknown:
+			
+			break;
+			
+		default:
+			
+			// call getProperty()
+			
+			if (object instanceof SimulationObject) {
+				SimulationObject simObj;
+				simObj = (SimulationObject) object;
+				result = simObj.getProperty(cluster, simPropName, valueName);
+			}
+			else if (object instanceof StateSimulationObject) {
+				StateSimulationObject stateSimObj;
+				stateSimObj = (StateSimulationObject) object;
+				result = stateSimObj.getProperty(cluster, simPropName, valueName);
+			}
+			else if (object instanceof ISavedValues) {
+				ISavedValues savedValue;
+				savedValue = (ISavedValues) object;
+				result = savedValue.getProperty(cluster, simPropName, valueName);
+			}
+			else if (object instanceof Event) {
+				
+			}
+			else if (object instanceof AbstractAction) {
+				
+			}
+			break;
+			
+		}
+
+		return result;
+		
 	}
 
-	public GetProperty(SimulationCluster cluster, String methodName, String propertyName) {
-		
-		super();
-		
-		setOperation(Expression_Function.property);
-		setValue(new Value(Type.simPropName, PropertyName.unknown));
-		
-		Expression exp1 = new Constant(new Value(Type.integer, cluster.getIndex()));
-		Expression exp2 = new Constant(new Value(Type.string, methodName));
-		Expression exp3 = new Constant(new Value(Type.string, propertyName));
-		
-		setExpression1(exp1);
-		setExpression2(exp2);
-		setExpression3(exp3);
-		setValid();
-			
-	}
+	
 
 }
