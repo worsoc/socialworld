2bdbbce2cdd226c6acb9187e3fd018b79fb5f861
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2020-08-07 09:27:36 +0000

    initializing test data: descriptions depends on EventType and EventReactionType

--- trunk/org.socialworld/src/org/socialworld/datasource/pool/EventReactionDescriptionPool.java
+++ trunk/org.socialworld/src/org/socialworld/datasource/pool/EventReactionDescriptionPool.java
@@ -29,24 +29,27 @@ import org.socialworld.calculation.FunctionByExpression;
 import org.socialworld.calculation.Value;
 import org.socialworld.calculation.descriptions.EventReactionDescription;
 import org.socialworld.calculation.expressions.CreateActionExpression;
-import org.socialworld.calculation.expressions.Nothing;
 import org.socialworld.core.EventType;
 
 public class EventReactionDescriptionPool extends DescriptionPool {
 
-	public static final int COUNT_FbE_TEST_ENTRIES = 4;		// Anzahl Testeintraege FunctionByExpression
 	
 	private static EventReactionDescriptionPool instance;
 	
 	private EventReactionDescription descriptions[];
-	private FunctionByExpression expressions[];
+	private List<List<FunctionByExpression>> expressions;
 	
 	private EventReactionDescriptionPool () {
 		
 		sizeDescriptionsArray = EventType.MAX_EVENT_TYPE * GaussPoolReactionType.CAPACITY_GPRT_ARRAY;
 		descriptions = new EventReactionDescription[sizeDescriptionsArray];
 
-		expressions = new FunctionByExpression[COUNT_FbE_TEST_ENTRIES];
+		expressions = new ArrayList<List<FunctionByExpression>>(sizeDescriptionsArray);
+		ArrayList<FunctionByExpression> nothing;
+		nothing = new  ArrayList<FunctionByExpression>();
+		for (int i = 0; i < sizeDescriptionsArray; i++) {
+			expressions.add(nothing);
+		}
 		
 		initialize();
 	}
@@ -76,21 +79,35 @@ public class EventReactionDescriptionPool extends DescriptionPool {
 
 	protected void initialize() {
 
+		int index;
+		
 		EventReactionDescription description;
-
+		List<FunctionByExpression> oneDescriptionExpressions;
+		
 		initializeWithTestData_FunctionByExpression();
 		initializeFromFile();
 
-		for (int index = 0; index < sizeDescriptionsArray; index++) {
-			
-			description = new EventReactionDescription();
-			
-			for (int i = 0; i < COUNT_FbE_TEST_ENTRIES; i++) {
-				description.addFunctionCreateReaction(expressions[i]);
+		for (int indexEventType = 0; indexEventType < EventType.MAX_EVENT_TYPE; indexEventType++) {
+		
+			for (int indexReactionType = 0; indexReactionType < GaussPoolReactionType.CAPACITY_GPRT_ARRAY; indexReactionType++) {
+				
+				index = indexEventType * GaussPoolReactionType.CAPACITY_GPRT_ARRAY + indexReactionType;
+				oneDescriptionExpressions = expressions.get(index);
+				
+				description = new EventReactionDescription();
+				
+				if (oneDescriptionExpressions != null) {
+					
+					for (int i = 0; i < oneDescriptionExpressions.size(); i++) {
+						description.addFunctionCreateReaction(oneDescriptionExpressions.get(i));
+					}
+					
+				}
+				
+				descriptions[index] = description;
+		
 			}
 			
-			descriptions[index] = description;
-	
 		}
 			
 	}
@@ -98,25 +115,64 @@ public class EventReactionDescriptionPool extends DescriptionPool {
 	
 	private void initializeWithTestData_FunctionByExpression() {
 		
-		
-		int 		expressionsCount = COUNT_FbE_TEST_ENTRIES;
-		List<List<String>> expressions = new ArrayList<List<String>>(expressionsCount);
+		int index;
 		List<String> lines;
-		Expression startExpression = Nothing.getInstance();
-
+		Expression startExpression;
+		List<FunctionByExpression> oneDescriptionExpressions;
 		
+		//candidatesMoveWalk(264)
+		oneDescriptionExpressions = new ArrayList<FunctionByExpression>();
 		lines = new ArrayList<String>(1);
 		lines.add("WENN 3 " + Value.VALUE_BY_NAME_EVENT_MOVE_VELOCITY + " > 11 & mood >= 60 & mood < 90 DANN <TYPE><Const>6</Const></TYPE><MODE><Const>67</Const></MODE><MINTIME><Now+N>1000</Now+N></MINTIME><MAXTIME><Now+N>10000</Now+N></MAXTIME><PRIORITY><Const>155</Const></PRIORITY><INTENSITY><MX+N>8;1.5;0</MX+N></INTENSITY><DURATION><Const>2000</Const></DURATION><TARGET><GetEvParm>" + Value.VALUE_BY_NAME_EVENT_CAUSER + "</GetEvParm></TARGET>");
-		expressions.add(lines);
-
+		startExpression = new CreateActionExpression(lines, CreateActionExpression.MODUS_CREATE_REACTION);
+		oneDescriptionExpressions.add(new FunctionByExpression(startExpression));
+		
+		index = EventType.candidatesMoveWalk.getIndex() * GaussPoolReactionType.CAPACITY_GPRT_ARRAY;
+		for (int i = 0; i < GaussPoolReactionType.CAPACITY_GPRT_ARRAY; i++) {
+			this.expressions.set(index + i, oneDescriptionExpressions);
+		}
+		
+		// candidatesMoveRun(265)
+		oneDescriptionExpressions = new ArrayList<FunctionByExpression>();
 		lines = new ArrayList<String>(1);
 		lines.add("WENN 3 " + Value.VALUE_BY_NAME_EVENT_MOVE_VELOCITY + " > 11 & mood >= 45 & mood < 60 DANN <TYPE><Const>7</Const></TYPE><MODE><Const>74</Const></MODE><MINTIME><Now+N>1000</Now+N></MINTIME><MAXTIME><Now+N>10000</Now+N></MAXTIME><PRIORITY><Const>162</Const></PRIORITY><INTENSITY><MX+N>8;0.1;0</MX+N></INTENSITY><DURATION><Const>2000</Const></DURATION><TARGET><GetEvParm>" + Value.VALUE_BY_NAME_EVENT_CAUSER + "</GetEvParm></TARGET>");
-		expressions.add(lines);
+		startExpression = new CreateActionExpression(lines, CreateActionExpression.MODUS_CREATE_REACTION);
+		oneDescriptionExpressions.add(new FunctionByExpression(startExpression));
+		
+		index = EventType.candidatesMoveRun.getIndex() * GaussPoolReactionType.CAPACITY_GPRT_ARRAY;
+		for (int i = 0; i < GaussPoolReactionType.CAPACITY_GPRT_ARRAY; i++) {
+			this.expressions.set(index + i, oneDescriptionExpressions);
+		}
 
+		// candidatesSayScream(337)
+		oneDescriptionExpressions = new ArrayList<FunctionByExpression>();
 		lines = new ArrayList<String>(1);
 		lines.add("WENN 3 " + Value.VALUE_BY_NAME_EVENT_SAY_LOUDNESS + " > 50 & mood >= 30 & mood < 45 DANN <TYPE><Const>3</Const></TYPE><MODE><Const>31</Const></MODE><MINTIME><Now+N>1000</Now+N></MINTIME><MAXTIME><Now+N>10000</Now+N></MAXTIME><PRIORITY><Const>160</Const></PRIORITY><INTENSITY><MX+N>8;1.3;23</MX+N></INTENSITY><DURATION><Const>2000</Const></DURATION>");
-		expressions.add(lines);
+		startExpression = new CreateActionExpression(lines, CreateActionExpression.MODUS_CREATE_REACTION);
+		oneDescriptionExpressions.add(new FunctionByExpression(startExpression));
+		
+		index = EventType.candidatesSayScream.getIndex() * GaussPoolReactionType.CAPACITY_GPRT_ARRAY;
+		for (int i = 0; i < GaussPoolReactionType.CAPACITY_GPRT_ARRAY; i++) {
+			this.expressions.set(index + i, oneDescriptionExpressions);
+		}
 
+		
+		// selfInventorySet(35)
+		oneDescriptionExpressions = new ArrayList<FunctionByExpression>();
+		lines = new ArrayList<String>(1);
+		lines.add("WENN 21 " + Value.VALUE_BY_NAME_EVENT_EQUIP_ITEMISEATABLE + " == false DANN <TYPE><Const>0</Const></TYPE><MODE><Const>3</Const></MODE><MINTIME><Now+N>1000</Now+N></MINTIME><MAXTIME><Now+N>2000</Now+N></MAXTIME><PRIORITY><Const>165</Const></PRIORITY><INTENSITY><MX+N>7;1;0</MX+N></INTENSITY><DURATION><Const>1000</Const></DURATION>");
+		startExpression = new CreateActionExpression(lines, CreateActionExpression.MODUS_CREATE_REACTION);
+		oneDescriptionExpressions.add(new FunctionByExpression(startExpression));
+		
+		index = EventType.selfInventorySet.getIndex() * GaussPoolReactionType.CAPACITY_GPRT_ARRAY;
+		for (int i = 0; i < GaussPoolReactionType.CAPACITY_GPRT_ARRAY; i++) {
+			this.expressions.set(index + i, oneDescriptionExpressions);
+		}
+
+		
+
+		
+		
 //		lines = new ArrayList<String>(1);
 //		lines.add("WENN hunger >= 45 & courage >= 45 DANN <TYPE><Const>4</Const></TYPE><MODE><Const>41</Const></MODE><MINTIME><Now+N>1000</Now+N></MINTIME><MAXTIME><Now+N>50000</Now+N></MAXTIME><PRIORITY><Const>100</Const></PRIORITY><INTENSITY><MX+N>7;1;0</MX+N></INTENSITY><DURATION><Const>1000</Const></DURATION>");
 //		expressions.add(lines);
@@ -124,20 +180,7 @@ public class EventReactionDescriptionPool extends DescriptionPool {
 //		lines = new ArrayList<String>(1);
 //		lines.add("WENN hunger >= 45 & courage >= 45 DANN <TYPE><Const>4</Const></TYPE><MODE><Const>44</Const></MODE><MINTIME><Now+N>1000</Now+N></MINTIME><MAXTIME><Now+N>50000</Now+N></MAXTIME><PRIORITY><Const>99</Const></PRIORITY><INTENSITY><MX+N>7;1;0</MX+N></INTENSITY><DURATION><Const>1000</Const></DURATION><ITEM><GetEvParm>" + Value.VALUE_BY_NAME_EVENT_CAUSER + "</GetEvParm></ITEM><INVENTORYPLACE><Const>3</Const></INVENTORYPLACE>");
 //		expressions.add(lines);
-		
-		
-		lines = new ArrayList<String>(1);
-		lines.add("WENN 21 " + Value.VALUE_BY_NAME_EVENT_EQUIP_ITEMISEATABLE + " == false DANN <TYPE><Const>0</Const></TYPE><MODE><Const>3</Const></MODE><MINTIME><Now+N>1000</Now+N></MINTIME><MAXTIME><Now+N>2000</Now+N></MAXTIME><PRIORITY><Const>165</Const></PRIORITY><INTENSITY><MX+N>7;1;0</MX+N></INTENSITY><DURATION><Const>1000</Const></DURATION>");
-		expressions.add(lines);
 
-		
-
-		for (int i = 0; i <  expressionsCount; i++) {
-			
-			startExpression = new CreateActionExpression(expressions.get(i), CreateActionExpression.MODUS_CREATE_REACTION);
-			this.expressions[i] = new FunctionByExpression(startExpression);
-
-		}
 		
 	}
 
