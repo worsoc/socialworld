0dfcbc1aef79ae1e22625330e660e9892af6a191
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2020-08-09 12:42:17 +0000

    Strings4Event as three dimensional String array

--- org.socialworld/src/org/socialworld/datasource/pool/Strings4EventType.java
+++ org.socialworld/src/org/socialworld/datasource/pool/Strings4EventType.java
@@ -8,11 +8,21 @@ import org.socialworld.core.EventType;
 public class Strings4EventType {
 
 	private EventType eventType;
-	private List<String> lines;
+	private List<List<List<String>>> lines;
 	
-	Strings4EventType(EventType eventType) {
+	// capacity is the range for the mapped type
+	// for example: how many reaction types are possible for event type
+	private int capacity;
+	
+	Strings4EventType(EventType eventType, int capacity) {
 		this.eventType = eventType;
-		this.lines = new ArrayList<String>();
+		this.capacity = capacity;
+		this.lines = new ArrayList<List<List<String>>>();
+		
+		for (int i = 0; i < capacity; i++) {
+			this.lines.add(new ArrayList<List<String>>());
+			this.lines.get(i).add(new ArrayList<String>());
+		}
 	}
 		
 	
@@ -21,11 +31,25 @@ public class Strings4EventType {
 		return this.eventType;
 	}
 	
-	List<String> getLines() {
-		return this.lines;
+	int getCapacity() {
+		return this.capacity;
 	}
 	
-	void add(String line) {
-		this.lines.add(line);
+	List<List<String>> getLines(int mainIndex) {
+		// main index ... the int value for the mapped type (reaction type, influence type, perception type ...)
+		if (mainIndex > 0 & mainIndex < this.capacity)
+			return this.lines.get(mainIndex);
+		else
+			return new ArrayList<List<String>>();
+	}
+	
+	void add(int mainIndex, int nr, String line) {
+		// main index ... the int value for the mapped type (reaction type, influence type, perception type ...)
+		if (mainIndex >= 0 & mainIndex < this.capacity) {
+			List<List<String>> forMainIndex = this.lines.get(mainIndex);
+			if (nr >= 0 & nr < forMainIndex.size()) {
+				forMainIndex.get(nr).add(line);
+			}
+		}
 	}
 }
