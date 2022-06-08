7545fa64147bf23c2bc883f41603bc8c1f6faf1a
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2022-06-08 08:43:40 +0000

    with column parse_input_ref_id

--- org.socialworld/src/org/socialworld/datasource/tablesPool/TablePoolParseInput.java
+++ org.socialworld/src/org/socialworld/datasource/tablesPool/TablePoolParseInput.java
@@ -7,11 +7,12 @@ import org.socialworld.datasource.mariaDB.Table;
 
 public class TablePoolParseInput extends Table {
 
-	public final  String 	ALL_COLUMNS 		=	" parse_input_id, parse_input_type, parse_input_string ";
+	public final  String 	ALL_COLUMNS 		=	" parse_input_id, parse_input_type, parse_input_ref_id, parse_input_string ";
 	public final  int 		SELECT_ALL_COLUMNS 	= 1;
 
 	int parse_input_id[];
 	int parse_input_type[];
+	int parse_input_ref_id[];
 	String parse_input_string[]; 		
 
 	@Override
@@ -54,6 +55,7 @@ public class TablePoolParseInput extends Table {
 
 		parse_input_id = new int[rowCount];
 		parse_input_type = new int[rowCount];
+		parse_input_ref_id = new int[rowCount];
 		parse_input_string = new String[rowCount];
 	
 
@@ -62,7 +64,8 @@ public class TablePoolParseInput extends Table {
 				
 				parse_input_id[row] = rs.getInt(1);
 				parse_input_type[row] = rs.getInt(2);
-				parse_input_string[row] = rs.getString(3);
+				parse_input_ref_id[row] = rs.getInt(3);
+				parse_input_string[row] = rs.getString(4);
 				
 				row++;
 			}
@@ -74,13 +77,13 @@ public class TablePoolParseInput extends Table {
 		
 	}
 
-	public void insert(int  parse_input_id, int  parse_input_type,  String parse_input_string ) {
+	public void insert(int  parse_input_id, int  parse_input_type, int  parse_input_ref_id, String parse_input_string ) {
 		String statement;
 			
 		if ((parse_input_id > 0) & (parse_input_type > 0)  & (parse_input_string.length() > 0)) {
 			
 			statement 	= "INSERT INTO swpool_parseinputs (" + ALL_COLUMNS + ") VALUES (" + 
-					parse_input_id + ", " + parse_input_type + ", '" + parse_input_string + "')";
+					parse_input_id + ", " + parse_input_type + ", " + parse_input_ref_id + ", '" + parse_input_string + "')";
 			
 			insert(statement);
 		}
@@ -103,6 +106,10 @@ public class TablePoolParseInput extends Table {
 
 	public int getType(int index) {
 		return parse_input_type[index];
+	}
+
+	public int getParseInputRefID(int index) {
+		return parse_input_ref_id[index];
 	}
 
 	public String getInputString(int index) {
