27277aa5cd3b61a5aa14ed6b050dfe5070af9b9e
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2022-11-19 17:32:23 +0000

    test clothes

---
+++ org.socialworld/src/org/socialworld/objects/concrete/clothes/trousers/TestTrousers.java
@@ -0,0 +1,26 @@
+package org.socialworld.objects.concrete.clothes.trousers;
+
+import org.socialworld.objects.State;
+import org.socialworld.objects.concrete.clothes.Trousers;
+
+public class TestTrousers extends Trousers {
+
+	@Override
+	protected int getLexemID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean checkObjectBelongsToGroup(short groupNumberSuffix) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected State getInitState(String stateClassName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
