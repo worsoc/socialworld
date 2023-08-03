7f6977665289eba7531489fc76696d0eb7430576
socialworld <socialworld@46caabca-0941-0410-bfc6-fabcf0f1e5d2>
2023-08-03 15:26:38 +0000

    IEnumProperty for property enumerations

--- org.socialworld/src/org/socialworld/attributes/properties/IEnumProperty.java
+++ org.socialworld/src/org/socialworld/attributes/properties/IEnumProperty.java
@@ -1,9 +1,10 @@
 package org.socialworld.attributes.properties;
 
+import org.socialworld.conversation.Lexem;
 
 public interface IEnumProperty {
 
 	public int getIndex();
-	
+	public Lexem getLexem();
 	
 }
