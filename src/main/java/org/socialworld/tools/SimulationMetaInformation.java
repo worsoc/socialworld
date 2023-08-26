4881063698469cc7f3e6bf6b5070a6719a9e173e
Mathias Sikos <sikosmathias@aol.com>
2023-08-26 10:51:28 +0200

    license entry added

--- src/main/java/org/socialworld/tools/SimulationMetaInformation.java
+++ src/main/java/org/socialworld/tools/SimulationMetaInformation.java
@@ -1,7 +1,7 @@
 
 /*
 * Social World
-* Copyright (C) 2014  Mathias Sikos
+* Copyright (C) 2022  Mathias Sikos
 *
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License
