ecd51a5f91a4fc44a314d42aaaa2818282c5c299
Mathias Sikos <sikosmathias@aol.com>
2023-09-12 21:11:14 +0200

    optimisation IntHashMap.getIDsPerLevel()

--- src/main/java/org/socialworld/collections/IntHashMap.java
+++ src/main/java/org/socialworld/collections/IntHashMap.java
@@ -33,9 +33,9 @@ public class IntHashMap {
 	
 	public Object get(int id) {
 		
-		byte[] idsPerLevel = getIDsPerLevel(id);
-		
-		IntHashMapNode node = this.startNode;
+ 		byte[] idsPerLevel = getIDsPerLevel(id);
+
+ 		IntHashMapNode node = this.startNode;
 		IntHashMapNode nextNode = null;
 			
 		for (int i = 0; i < 8 ; i++) {
@@ -54,43 +54,17 @@ public class IntHashMap {
 	
 	private byte[] getIDsPerLevel(int id) {
 		
-		String idHex = Integer.toHexString(id);
-		idHex = "00000000" + idHex;
-		idHex = idHex.substring(idHex.length() - 8);
-		
-		byte[] idsPerLevel = transformToByteArray(idHex);
-		
-		return idsPerLevel;
-	}
-	
-	private byte[] transformToByteArray(String idHex) {
-		
-		char[] chars = idHex.toCharArray();
-		byte [] result = new byte[chars.length];
-		
-		int index = 0;
-		for (char c : chars) {
-			switch (c) {
-				case '0' : result[index] = 0; break;
-				case '1' : result[index] = 1; break;
-				case '2' : result[index] = 2; break;
-				case '3' : result[index] = 3; break;
-				case '4' : result[index] = 4; break;
-				case '5' : result[index] = 5; break;
-				case '6' : result[index] = 6; break;
-				case '7' : result[index] = 7; break;
-				case '8' : result[index] = 8; break;
-				case '9' : result[index] = 9; break;
-				case 'a' : result[index] = 10; break;
-				case 'b' : result[index] = 11; break;
-				case 'c' : result[index] = 12; break;
-				case 'd' : result[index] = 13; break;
-				case 'e' : result[index] = 14; break;
-				case 'f' : result[index] = 15; break;
-				default: result[index] = 0;
-			}
-			index++;
+ 		int rest = id;
+		byte[] idsPerLevel = new byte[8];
+		byte idPerLevel;
+		for (int i = 7; i >= 0; i--) {
+			idPerLevel = (byte) (rest % 16);
+			idsPerLevel[i]= idPerLevel;
+			rest = rest >> 4;
 		}
-		return result;
+		
+ 		return idsPerLevel;
 	}
+
+
 }
