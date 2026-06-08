07283755c4c7cbf2cdeb5c2b0096c595aa6d9d88
Mathias Sikos <sikosmathias@aol.com>
2026-06-08 12:17:21 +0200

    more sleep time for talk and knowledge calculator (10ms) closes #19

--- src/main/java/org/socialworld/core/SocialWorldThread.java
+++ src/main/java/org/socialworld/core/SocialWorldThread.java
@@ -28,9 +28,9 @@ public class SocialWorldThread extends Thread implements IObjectReceiver{
 
 	public static final int SLEEPTIME_ACTION_CREATOR = 2;
 	public static final int SLEEPTIME_ATTRIBUTE_CALCULATOR = 5;
-	public static final int SLEEPTIME_KNOWLEDGE_CALCULATOR = 5;
 	public static final int SLEEPTIME_POSITION_CALCULATOR = 5;
-	public static final int SLEEPTIME_TALK_CALCULATOR = 5;
+	public static final int SLEEPTIME_KNOWLEDGE_CALCULATOR = 10;
+	public static final int SLEEPTIME_TALK_CALCULATOR = 10;
 	
 	/**
 	 * says whether the thread is running or not
